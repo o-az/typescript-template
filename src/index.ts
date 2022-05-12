09c6dbb9281b35dcfdf4a75bdc3f8d4a4d1e1ba8
o-az <23618431+o-az@users.noreply.github.com>
2022-05-12 00:54:47 -0500

    remove commented code blocks

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,3 @@ export function main() {
   return environment.MY_ENV_VARIABLE
 }
 console.log(main())
-// // yarn tsnode src/index.ts --watch
-// const getPosts = await fetch('https://jsonplaceholder.typicode.com/posts')
-
-// console.log(getPosts.json())
