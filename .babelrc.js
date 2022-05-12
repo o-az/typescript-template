1c12b4a2f9647bb1525635f2305ea5be12a5cb9e
o-az <23618431+o-az@users.noreply.github.com>
2022-05-12 00:54:27 -0500

    added gitattributes and license

--- .babelrc.js
+++ .babelrc.js
@@ -1,6 +1,5 @@
 /** @type {import('@types/babel-core').TransformOptions} */
 module.exports = {
   presets: [['@babel/preset-env', { modules: false, targets: { node: true } }], '@babel/preset-typescript'],
-  ignore: ['node_modules/**'],
- // sourceType: 'module',
+  ignore: ['node_modules'],
 }
